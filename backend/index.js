5a7eff85463ab2ca6ddba905a04d4545f0e1ba26
Vansh Kumar <vansh200622@gmail.com>
2026-07-25 23:37:35 +0530

    add option cors

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.options('*', cors());
 app.get("/", (req, res) => {
   res.send("This is a new Project");
 })
