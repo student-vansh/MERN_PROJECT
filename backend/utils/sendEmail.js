d4b5f33f734b47573e019c56a7c863fe6df0a967
Vansh Kumar <vansh200622@gmail.com>
2026-07-26 01:11:56 +0530

    changes smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,10 +3,7 @@ const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
