1bd02f9fb73a7a46d15bd30465105075827e0ad5
Vansh Kumar <vansh200622@gmail.com>
2026-07-26 00:18:29 +0530

    add dns

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,4 +1,9 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// Force IPv4 resolution first, because Render.com's environment might attempt IPv6 
+// which can result in ENETUNREACH errors when connecting to Gmail's SMTP servers.
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
