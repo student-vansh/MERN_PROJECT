52205f5d0fa4e1d41c95f9ad5b71b100e38b8991
Vansh Kumar <vansh200622@gmail.com>
2026-07-26 00:36:37 +0530

    add smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
-
+console.log(process.env.EMAIL_USER);
+console.log(!!process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true sirf port 465 ke liye
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
